726910b333b64bb1b8b5b9be89306572b0522389
NikolayShubko <120208380+NikolayShubko@users.noreply.github.com>
2023-01-16 20:39:38 +0200

    Add files via upload

---
+++ hw4_Shubko/js/script.js
@@ -0,0 +1,5 @@
+//ask user amount of hours and convert to seconds
+let hours=prompt("Enter amount of hours")
+const secondsInHour = 3600
+let userHoursInSeconds = +hours * secondsInHour
+alert(`Seconds in ${hours} hours = ${userHoursInSeconds}`)
---
+++ hw5_Shubko/js/script.js
@@ -0,0 +1,6 @@
+//ask user 3 numbers and find average
+let firstNumber=prompt("Enter first number")
+let secondNumber=prompt("Enter second number")
+let thirdNumber=prompt("Enter third number")
+let average = (+firstNumber + +secondNumber + +thirdNumber) / 3
+alert(`Average of three numbers ${average}`)
