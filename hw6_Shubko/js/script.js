6260a989e96f509f79baf0bf297c2f5873147b81
NikolayShubko <120208380+NikolayShubko@users.noreply.github.com>
2023-01-21 18:24:19 +0200

    Add files via upload

---
+++ hw6_Shubko/js/script.js
@@ -0,0 +1,9 @@
+//ask user 3 numbers
+const firstNumber=prompt("Enter first number")
+const secondNumber=prompt("Enter second number")
+const thirdNumber=prompt("Enter third number")
+alert(firstNumber + secondNumber + thirdNumber)
+
+//number by digits
+const number = prompt("Enter 5 digits number")
+alert(`${number[0]} ${number[1]} ${number[2]} ${number[3]} ${number[4]}`)
