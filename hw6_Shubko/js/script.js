b967ba4e0c19d06b70ebfdf46d9a99ae244986d9
NikolayShubko <120208380+NikolayShubko@users.noreply.github.com>
2023-01-21 18:24:11 +0200

    Delete hw6_Shubko directory

--- hw6_Shubko/js/script.js
+++
@@ -1,9 +0,0 @@
-//ask user 3 numbers
-const firstNumber=prompt("Enter first number")
-const secondNumber=prompt("Enter second number")
-const thirdNumber=prompt("Enter third number")
-alert(`${firstNumber} ${secondNumber} ${thirdNumber}`)
-
-//number by digits
-const number = prompt("Enter 5 digits number")
-alert(`${number.charAt(0)} ${number.charAt(1)} ${number.charAt(2)} ${number.charAt(3)} ${number.charAt(4)}`)
