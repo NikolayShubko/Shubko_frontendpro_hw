648dca3fbcfd04d826a0c301a653abb535fe9900
NikolayShubko <120208380+NikolayShubko@users.noreply.github.com>
2023-01-23 22:12:30 +0200

    Add files via upload

---
+++ hw11_Shubko/js/script.js
@@ -0,0 +1,65 @@
+//1
+// let numbers = ''
+// for(let i = 20; i <= 30; i+=0.5){
+//     numbers += i + ' '
+// }
+// alert(numbers)
+
+
+// //2
+// const currencyRate = 27
+// let result = ''
+// for(let i = 10; i <= 100; i+=10){
+//     i === 100 ? result += currencyRate * i + ' Доларів': result += currencyRate * i + ', '
+// }
+// alert(result)
+
+//3
+// const someNumber = prompt("Enter some number")
+// let someSquares = ''
+// for(let i = 1; i <= 100; i++ ){
+//     if( i**2 < someNumber){
+//         someSquares += i + ' '
+//     }
+// }
+// alert(someSquares)
+
+//4
+// const someNumber = Number(prompt("Enter some number"))
+// let simpleNumber =true;
+// if(someNumber === 1){
+//     alert("Not this one")
+// }
+// else if(someNumber>1){
+//     for(i = 2; i < someNumber;i++){
+//         if(someNumber % i === 0){
+//             simpleNumber = false
+//             break;
+//         }
+//         else{
+//             simpleNumber = true
+//         }
+//     }
+
+//     if(simpleNumber){
+//         alert("Simple")
+//     }
+//     else{
+//         alert("not simple")
+//     }
+// }
+// else{
+//     alert('Number less than 1')
+// }
+//5
+const someNumberSecond = Number(prompt("Enter some number"))
+const startNumber = 3;
+let fromNumber = ''
+for(let i=0; i<someNumberSecond;i++){
+    if(Math.pow(startNumber, i) === someNumberSecond){
+        fromNumber +=`You get this number by ${startNumber}^${i} = ${someNumberSecond}`
+        break
+
+    }
+}
+alert(fromNumber)
