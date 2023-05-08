5dc7608e5ab6ab68de5680a7ee024ac58c3e8fc0
NikolayShubko <120208380+NikolayShubko@users.noreply.github.com>
2023-05-08 21:48:34 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+import Smiles from "./components/Smiles/Smiles";
+function App() {
+  return (
+    <div className="App">
+      <Smiles />
+    </div>
+  );
+}
+
+export default App;
