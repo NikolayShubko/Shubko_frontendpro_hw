05d62febe6960231c293a55743db6b2b2e4307fa
NikolayShubko <120208380+NikolayShubko@users.noreply.github.com>
2023-05-12 23:13:50 +0300

    Delete hw51_Shubko directory

--- hw51_Shubko/src/components/Modal/Modal.js
+++
@@ -1,8 +0,0 @@
-import "./Modal.css";
-const Modal = ({ isShown, children }) => {
-  if (isShown) {
-    return <div className="modal-holder">{children}</div>;
-  }
-  return;
-};
-export default Modal;
