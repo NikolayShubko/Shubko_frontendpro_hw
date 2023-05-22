495ec2c47fba23dbe7cc0f563b9faf34f0b332b3
NikolayShubko <120208380+NikolayShubko@users.noreply.github.com>
2023-05-22 22:08:53 +0300

    Add files via upload

---
+++ hw53_Shubko/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import { Forma } from "./components/Forma/Forma";
+
+function App() {
+  return (
+    <div className="App">
+      <Forma />
+    </div>
+  );
+}
+
+export default App;
